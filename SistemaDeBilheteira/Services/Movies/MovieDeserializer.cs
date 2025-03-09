70bf6a9ba53ddece79342a404c96e7d83222f231
Fabio11112 <2123422@student.uma.pt>
2025-03-09 12:08:43 +0000

    feat: Adding use of API for films

---
+++ SistemaDeBilheteira/Services/Movies/MovieDeserializer.cs
@@ -0,0 +1,17 @@
+﻿namespace SistemaDeBilheteira.Services.Movies;
+
+using Newtonsoft.Json;
+
+public class MovieDeserializer
+{
+    public MovieResponse FetchPopularMovies(string link)
+    {
+        using (var client = new HttpClient())
+        {
+            var endpoint = new Uri(link + "?api_key=" + "88c2011578e30a692349e50d3119755c");
+            var response = client.GetAsync(endpoint).Result;
+            string jsonString = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<MovieResponse>(jsonString);
+        }
+    }
+}
---
+++ SistemaDeBilheteira/Services/Movies/MovieResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace SistemaDeBilheteira.Services.Movies;
+
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class MovieResponse
+{
+    [JsonProperty("page")]
+    public int Page { get; set; }
+
+    [JsonProperty("results")]
+    public List<Movie> Results { get; set; }
+
+    [JsonProperty("total_pages")]
+    public int TotalPages { get; set; }
+
+    [JsonProperty("total_results")]
+    public int TotalResults { get; set; }
+}
