26047aff376ae216770c80711fec3b152b5c6016
Fabio11112 <2123422@student.uma.pt>
2025-04-15 16:41:29 +0100

    Added the options for the User Profile Page

--- SistemaDeBilheteira/Program.cs
+++ SistemaDeBilheteira/Program.cs
@@ -1,4 +1,5 @@
 using SistemaDeBilheteira.Components;
+using Toolbelt.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseCssLiveReload(); 
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
