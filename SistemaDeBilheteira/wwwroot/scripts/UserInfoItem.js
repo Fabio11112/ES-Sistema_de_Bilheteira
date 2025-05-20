d42a47aa45418f005af87306ee0e1614db85d578
RaulRodrigues06 <raulandre98@hotmail.com>
2025-05-20 12:07:08 +0100

    Feat: Styles in Card and only numbers on card number

---
+++ SistemaDeBilheteira/wwwroot/scripts/UserInfoItem.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const element = document.getElementById("cardNumber");
+    if (element) {
+        element.addEventListener('keydown', function(event) {
+     BlockLetters(event);
+});
+}
+});
+window.BlockLetters = function (e) {
+    // Lista de teclas permitidas: números y teclas de control
+    var allowedKeys = [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "Backspace", "Delete", "Tab", "ArrowLeft", "ArrowRight",
+    ];
+    if (!allowedKeys.includes(e.key)) {
+        e.preventDefault();
+    }
+}
+window.FormatCardNumber = function (input) {
+    let value = input.value.replace(/\D/g, '').substring(0, 16); // só números, máximo 16 dígitos
+    value = value.replace(/(.{4})/g, '$1 ').trim();// espaço a cada 4 dígitos
+    input.value = value; // Atualiza o valor do input
+}
+
+  
+
