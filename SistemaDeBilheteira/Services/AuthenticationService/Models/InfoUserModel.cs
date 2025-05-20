20a1d9782d9e53e2ba7408fe814750ffd48646c1
RaulRodrigues06 <raulandre98@hotmail.com>
2025-05-20 13:51:16 +0100

    Feat: EditForm for UserInfo

---
+++ SistemaDeBilheteira/Services/AuthenticationService/Models/InfoUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaDeBilheteira.Services.AuthenticationService.Models;
+
+public class InfoUserModel : IModel
+{
+    [Required]
+    public string EmailAddress { get; set; } = string.Empty;
+    [Required]
+    public string OldPassword { get; set; } = string.Empty;
+    [Required]
+    public string NewPassword { get; set; } = string.Empty;
+    [Required]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
