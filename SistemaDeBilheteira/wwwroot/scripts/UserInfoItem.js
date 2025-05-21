ce42a8bcbe135f54f3ca0d65e9b6fba809371110
RaulRodrigues06 <raulandre98@hotmail.com>
2025-05-21 09:50:13 +0100

    Feat: Visibility password

--- SistemaDeBilheteira/wwwroot/scripts/UserInfoItem.js
+++ SistemaDeBilheteira/wwwroot/scripts/UserInfoItem.js
@@ -7,7 +7,6 @@
 }
 });
 window.BlockLetters = function (e) {
-    // Lista de teclas permitidas: números y teclas de control
     var allowedKeys = [
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
         "Backspace", "Delete", "Tab", "ArrowLeft", "ArrowRight",
@@ -17,10 +16,28 @@ window.BlockLetters = function (e) {
     }
 }
 window.FormatCardNumber = function (input) {
-    let value = input.value.replace(/\D/g, '').substring(0, 16); // só números, máximo 16 dígitos
-    value = value.replace(/(.{4})/g, '$1 ').trim();// espaço a cada 4 dígitos
-    input.value = value; // Atualiza o valor do input
+    let value = input.value.replace(/\D/g, '').substring(0, 16); // Max of 16 numbers
+    value = value.replace(/(.{4})/g, '$1 ').trim();// space betwen every 4 digits
+    input.value = value; 
 }
 
+
+window.FormatCVVNumber = function (input) {
+    let value = input.value.replace(/\D/g, '').substring(0, 3); // max of 3 numbers
+    input.value = value; 
+}
+
+window.togglePassword = function(inputId, iconElement) {
+    const input = document.getElementById(inputId);
+    if (!input) return;
+
+    if (input.type === "password") {
+        input.type = "text";
+        iconElement.src = "images/icons/eye-closed.svg";
+    } else {
+        input.type = "password";
+        iconElement.src = "images/icons/eye-icon.svg"; 
+    }
+}
   
 
