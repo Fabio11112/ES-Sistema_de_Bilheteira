4c4d91c045c12d73e7ba04df04aed3f5df6e5360
Fabio11112 <2123422@student.uma.pt>
2025-07-05 00:24:23 +0100

    feat: added the capacity for navbar to update the cart items elements

---
+++ SistemaDeBilheteira/Services/CartService.cs
@@ -0,0 +1,33 @@
+﻿using SistemaDeBilheteira.Components.Pages;
+using SistemaDeBilheteira.Services.AuthenticationService.IService;
+using SistemaDeBilheteira.Services.Database.Entities;
+using SistemaDeBilheteira.Services.Database.Entities.ShoppingCart;
+using SistemaDeBilheteira.Services.IService.ServiceManager;
+using SistemaDeBilheteira.Services.IService;
+
+namespace SistemaDeBilheteira.Services;
+
+public class CartService
+{
+    public int ItemCount { get; set; }
+    public AppUser User { get; set; }
+    public event Action? OnChange;
+    
+    
+    public void UpdateCart(int itemCount)
+    {
+        ItemCount = itemCount;
+        NotifyStateHasChanged();
+    }
+
+    public void UpdateUser(AppUser user)
+    {
+        User = user;
+    }
+
+    private void NotifyStateHasChanged()
+    {
+        OnChange?.Invoke();
+    }
+    
+}
